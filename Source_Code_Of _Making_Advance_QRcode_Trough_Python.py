9a7ae54dd743d55d0e125996a0e7758bb399a405
Rabban Ali <105205500+RabbanAli1122@users.noreply.github.com>
2022-09-03 16:32:10 +0500

    Update Source_Code_Of _Making_Advance_QRcode_Trough_Python.py

--- Source_Code_Of _Making_Advance_QRcode_Trough_Python.py
+++ Source_Code_Of _Making_Advance_QRcode_Trough_Python.py
@@ -2,7 +2,7 @@ import qrcode
 from PIL import Image
 qr=qrcode.QRCode(version=6,
                   box_size=10, border=10)
-qr.add_data("https://www.youtube.com/watch?v=OKuiyX5k6zg")
+qr.add_data("https://github.com/RabbanAli1122")
 qr.make(fit=True)
 img=qr.make_image(fill_color="white",back_color="black")
 img.save("python_20_projects.png")
